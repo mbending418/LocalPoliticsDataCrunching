31d4f3df82fb2e187b586afe991a83679a122d23
Michael Bending <michael.a.bending@gmail.com>
2025-03-24 00:08:44 -0400

    refactored young voter finder

--- local_politics/young_dems.py
+++ local_politics/young_dems.py
@@ -1,10 +1,31 @@
 from utils.filter_dataframe import filter_by_mapping
+from typing import Optional
 import os
 import pandas as pd
+
 
 BIRTH_YEAR = "birth_date"
 PARTY = "party"
 CITY = "city"
+
+
+def find_dems_born_after_target_year(df, year: int, city: Optional[str]):
+    """
+    searches a Pandas DataFrame representing voters for
+    all democratic voters born after a target year
+    Optionally restricting the search to a given city
+
+    :param df: Pandas DataFrame of voters
+    :param year: target birth year
+    :param city: city to narrow the search to
+    :return:
+    """
+    voter_filter = {
+        PARTY: "DEM",
+        CITY: city,
+    }
+    filtered_df = filter_by_mapping(df, voter_filter)
+    return filtered_df[filtered_df[BIRTH_YEAR] > year]
 
 
 def find_young_dems(boe_voter_csv):
@@ -18,14 +39,9 @@ def find_young_dems(boe_voter_csv):
     file_base = os.path.splitext(boe_voter_csv)[0]
 
     boe_df = pd.read_csv(boe_voter_csv)
-    voter_filter = {
-        PARTY: "DEM",
-        CITY: "CLEVELAND HTS",
-    }
-    ch_dems_df = filter_by_mapping(boe_df, voter_filter)
 
-    under_35_ch_dems = ch_dems_df[ch_dems_df[BIRTH_YEAR] > 1989]
+    under_35_ch_dems = find_dems_born_after_target_year(boe_df, year=1989, city="CLEVELAND HTS")
     under_35_ch_dems.to_csv(f"{file_base}_under_35_dems.csv")
 
-    under_25_ch_dems = ch_dems_df[ch_dems_df[BIRTH_YEAR] > 1999]
+    under_25_ch_dems = find_dems_born_after_target_year(boe_df, year=1999, city="CLEVELAND HTS")
     under_25_ch_dems.to_csv(f"{file_base}_under_25_dems.csv")
