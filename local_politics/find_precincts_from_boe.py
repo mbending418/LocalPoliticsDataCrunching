4880e8d34343882076fff9457a23cc7d04c3eefc
Michael Bending <michael.a.bending@gmail.com>
2025-03-24 14:21:28 -0400

    refactored out voter matching from precinct search in find_precincts_from_boe.py

--- local_politics/find_precincts_from_boe.py
+++ local_politics/find_precincts_from_boe.py
@@ -1,9 +1,12 @@
-from utils.filter_dataframe import filter_by_column_value
+from typing import Optional, Literal, Union, List, Any
 import os
 import pandas as pd
 
+PartyType = Literal["DEM", "REP", "NOPTY"]
+
 # standardized column headers from board of elections
 CITY = "city"
+PARTY = "party"
 NAME_LAST = "name_last"
 NAME_FIRST = "name_first"
 WARD = "ward"
@@ -19,6 +22,32 @@ FULL_ADDRESS = "full_address"
 NAME_LAST_VOTER_CSV = "Last Name"
 NAME_FIRST_VOTER_CSV = "First Name"
 ADDRESS_CSV = "Address"
+
+
+def add_precincts_to_voter_df(voter_df, boe_df, match_on: Union[Any, List[Any]],
+                              city: Optional[str] = None,
+                              party: Optional[PartyType] = None):
+    """
+
+    :param voter_df: df of voter data
+    :param boe_df:  df of voter data from boe (that includes precinct information)
+    :param match_on: which columns to use to match voters from the boe_df to the voter_df
+    :param city: optionally restrict boe search to a given city
+    :param party: optionally restrict boe search to a particular politics party
+    :return:
+    """
+    if city is not None:
+        boe_df = boe_df[boe_df[CITY] == city]
+    if party is not None:
+        boe_df = boe_df[boe_df[PARTY] == party]
+
+    if not isinstance(match_on, list):
+        match_on = [match_on]
+    boe_slice = match_on + [WARD, PRECINCT]
+
+    return voter_df.merge(boe_df[boe_slice], how='left',
+                          left_on=match_on,
+                          right_on=match_on)
 
 
 def add_precincts_by_name(voter_csv, boe_voter_csv):
@@ -41,12 +70,11 @@ def add_precincts_by_name(voter_csv, boe_voter_csv):
     voter_df[NAME_FIRST] = voter_df[NAME_FIRST_VOTER_CSV].astype(str).str.upper()
 
     boe_df = pd.read_csv(boe_voter_csv)
-    # narrow down the search to cleveland heights
-    ch_df = filter_by_column_value(boe_df, column=CITY, value="CLEVELAND HTS")
 
-    merged_df = voter_df.merge(ch_df[[NAME_LAST, NAME_FIRST, WARD, PRECINCT]], how='left',
-                               left_on=[NAME_LAST, NAME_FIRST],
-                               right_on=[NAME_LAST, NAME_FIRST])
+    merged_df = add_precincts_to_voter_df(voter_df=voter_df,
+                                          boe_df=boe_df,
+                                          match_on=[NAME_LAST, NAME_FIRST],
+                                          city="CLEVELAND HTS")
 
     merged_df[[NAME_LAST_VOTER_CSV, NAME_FIRST_VOTER_CSV, ADDRESS_CSV, WARD, PRECINCT]].to_csv(
         f"{file_base}"f"_added_precinct_by_name.csv")
@@ -71,16 +99,15 @@ def add_precincts_by_address(voter_csv, boe_voter_csv):
     voter_df[FULL_ADDRESS] = voter_df[ADDRESS_CSV].astype(str).str.upper()
 
     boe_df = pd.read_csv(boe_voter_csv)
-    # narrow down the search to cleveland heights
-    ch_df = filter_by_column_value(boe_df, column=CITY, value="CLEVELAND HTS")
 
     # merge columns with address info into one column
-    ch_df[FULL_ADDRESS] = (ch_df[HOUSE_NUMBER].astype(str) + " " + ch_df[PREFIX_DIRECTION].fillna("") + " "
-                           + ch_df[STREET]).replace("  ", " ", regex=True)
+    boe_df[FULL_ADDRESS] = (boe_df[HOUSE_NUMBER].astype(str) + " " + boe_df[PREFIX_DIRECTION].fillna("") + " "
+                            + boe_df[STREET]).replace("  ", " ", regex=True)
 
-    merged_df = voter_df.merge(ch_df[[FULL_ADDRESS, WARD, PRECINCT]], how='left',
-                               left_on=[FULL_ADDRESS],
-                               right_on=[FULL_ADDRESS])
+    merged_df = add_precincts_to_voter_df(voter_df=voter_df,
+                                          boe_df=boe_df,
+                                          match_on=FULL_ADDRESS,
+                                          city="CLEVELAND HTS")
 
     merged_df[[NAME_LAST_VOTER_CSV, NAME_FIRST_VOTER_CSV, ADDRESS_CSV, WARD, PRECINCT]].to_csv(
         f"{file_base}"f"_added_precinct_by_address.csv")
